ef16ee4238d6603a5e7a30f18cd7924bab520336
Geek-ubaid <usmaniubaid@gmail.com>
2020-06-22 20:41:27 +0530

    result.json file location fixed

--- forest_utils/export_keras.py
+++ forest_utils/export_keras.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+import os
+
 import json
 import gdown
 
@@ -9,12 +11,30 @@ import tensorflow as tf
 
 class ModelFromH5(object):
     
-    def __init__(self, output = 'model.h5'):
+    def __init__(self, output = 'model.h5', config = 'result.json'):
         super().__init__()
         
         self.base_url = 'https://drive.google.com/uc?id='
-        self.url_id = self.get_complete_url(json.load(open('result.json','r'))["Link"])
+        self.url_id = self.check_for_json(config)
+        self.check_for_url_id(self.url_id)
         self.output = output
+        
+    def check_for_url_id(self, url):
+        if url!=False and url!='':
+            print('Link found !!')
+        else:
+            return
+        
+    def check_for_json(self, path):
+        if os.path.isfile(path):
+            try:
+                link = json.load(open(path))["Link"]
+            except:
+                print("Link not found!!")
+                return False
+        else:
+            print("File do not exists")
+            return False
         
     def get_complete_url(self, url):
         split_url = url.split('/')
