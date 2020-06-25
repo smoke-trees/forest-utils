5e424c694e99016fd737479a3094e64649ba0138
Geek-ubaid <usmaniubaid@gmail.com>
2020-06-25 14:53:36 +0530

    starting woth version 0.0.8

--- setup.py
+++ setup.py
@@ -1,9 +1,8 @@
 import os
+import re
 import codecs
 
-from setuptools import setup, find_packages
-
-REQUIREMENTS = ['gdown==3.11.1', 'requests==2.24.0', 'tensorflow', 'spacy', 'pandas']
+from setuptools import setup
 
 def read(rel_path):
     here = os.path.abspath(os.path.dirname(__file__))
@@ -18,33 +17,20 @@ def get_version(rel_path):
     else:
         raise RuntimeError("Unable to find version string.")
 
-this_directory = os.path.abspath(os.path.dirname(__file__))
 
-with open(os.path.join(this_directory, 'README.md'), encoding='utf-8') as f:
-    long_description = f.read()
-
+# Metadata goes in setup.cfg. These are here for GitHub's dependency graph.
 setup(
-        name="forest_utils",
-        version=get_version('VERSION.txt'),
-        description="Package for SmokeTrees model zoo",
-        long_description=get_long_description(),
-        url="https://github.com/smoke-trees/forest-utils",
-        author="SmokeTrees",
-        author_email=" info@smoketrees.dev",
-        python_requires='>=3.4',
-        packages=find_packages(include=[
-            "forest_utils"
-            "forest_utils.*"
-        ], exclude=["test.*, test"]),
-        include_package_data=True,
-           classifiers=[
-            "Development Status :: 4 - Beta",
-            "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
-            "Programming Language :: Python :: 3",
-            "Programming Language :: Python :: 3 :: Only",
-            "Programming Language :: Python :: 3.6",
-            "Programming Language :: Python :: Implementation :: CPython"
-        ],
-        install_requires=REQUIREMENTS,
-        keywords='utils package modelzoo'
+    name="forest-utils",
+    version=get_version('VERSION.txt'),
+    author="Smoketrees",
+    author_email=" info@smoketrees.dev",
+    install_requires=[
+        'gdown==3.11.1',
+        'requests==2.24.0',
+        'tensorflow',
+        'spacy',
+        'pandas',
+        'click'
+    ],
 )
+
